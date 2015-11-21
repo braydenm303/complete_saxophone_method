47291944b7dbdb9f990b1f565d2f9b1cde045762
Brayden Morris <braydenm303@gmail.com>
2015-11-21 05:43:18 -0700

    Add built website

---
+++ saxophone/images.pl
@@ -0,0 +1,318 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/includegraphics{b4slashlily-baaec67f-3};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="539" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img49.png"
+ ALT="\includegraphics{b4/lily-baaec67f-3}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-4};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="539" HEIGHT="68" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-4}">|; 
+
+$key = q/includegraphics{04slashlily-27b9a431-1};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="532" HEIGHT="59" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img18.png"
+ ALT="\includegraphics{04/lily-27b9a431-1}">|; 
+
+$key = q/includegraphics{daslashlily-e1c6fba5-8};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="545" HEIGHT="66" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img44.png"
+ ALT="\includegraphics{da/lily-e1c6fba5-8}">|; 
+
+$key = q/includegraphics{7cslashlily-79db51c2-2};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="532" HEIGHT="59" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img29.png"
+ ALT="\includegraphics{7c/lily-79db51c2-2}">|; 
+
+$key = q/includegraphics{b4slashlily-baaec67f-6};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="545" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img52.png"
+ ALT="\includegraphics{b4/lily-baaec67f-6}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-13};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="546" HEIGHT="78" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-13}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-12};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="547" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-12}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-7};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="544" HEIGHT="68" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-7}">|; 
+
+$key = q/includegraphics{7cslashlily-79db51c2-3};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="538" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img30.png"
+ ALT="\includegraphics{7c/lily-79db51c2-3}">|; 
+
+$key = q/includegraphics{daslashlily-e1c6fba5-7};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="545" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img43.png"
+ ALT="\includegraphics{da/lily-e1c6fba5-7}">|; 
+
+$key = q/includegraphics{b4slashlily-baaec67f-5};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="545" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img51.png"
+ ALT="\includegraphics{b4/lily-baaec67f-5}">|; 
+
+$key = q/includegraphics{64slashlily-a4d36519-2};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="532" HEIGHT="59" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img21.png"
+ ALT="\includegraphics{64/lily-a4d36519-2}">|; 
+
+$key = q/includegraphics{b4slashlily-baaec67f-4};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="538" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img50.png"
+ ALT="\includegraphics{b4/lily-baaec67f-4}">|; 
+
+$key = q/includegraphics{64slashlily-a4d36519-5};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="544" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img24.png"
+ ALT="\includegraphics{64/lily-a4d36519-5}">|; 
+
+$key = q/includegraphics{b4slashlily-baaec67f-1};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="115" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img47.png"
+ ALT="\includegraphics{b4/lily-baaec67f-1}">|; 
+
+$key = q/includegraphics{38slashlily-c18ae63d-3};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="539" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img27.png"
+ ALT="\includegraphics{38/lily-c18ae63d-3}">|; 
+
+$key = q/includegraphics{93slashlily-c3f88607-1};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="532" HEIGHT="138" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\includegraphics{93/lily-c3f88607-1}">|; 
+
+$key = q/includegraphics{daslashlily-e1c6fba5-4};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="538" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img40.png"
+ ALT="\includegraphics{da/lily-e1c6fba5-4}">|; 
+
+$key = q/includegraphics{64slashlily-a4d36519-1};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="111" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img20.png"
+ ALT="\includegraphics{64/lily-a4d36519-1}">|; 
+
+$key = q/includegraphics{7cslashlily-79db51c2-4};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="539" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img31.png"
+ ALT="\includegraphics{7c/lily-79db51c2-4}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-3};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="538" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-3}">|; 
+
+$key = q/includegraphics{7cslashlily-79db51c2-8};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="546" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img35.png"
+ ALT="\includegraphics{7c/lily-79db51c2-8}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-16};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="546" HEIGHT="69" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img17.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-16}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-11};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="546" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-11}">|; 
+
+$key = q/includegraphics{7cslashlily-79db51c2-1};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="121" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img28.png"
+ ALT="\includegraphics{7c/lily-79db51c2-1}">|; 
+
+$key = q/includegraphics{38slashlily-c18ae63d-1};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="115" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img25.png"
+ ALT="\includegraphics{38/lily-c18ae63d-1}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-2};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="532" HEIGHT="65" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-2}">|; 
+
+$key = q/includegraphics{daslashlily-e1c6fba5-9};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="544" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img45.png"
+ ALT="\includegraphics{da/lily-e1c6fba5-9}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-9};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="546" HEIGHT="68" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-9}">|; 
+
+$key = q/includegraphics{7cslashlily-79db51c2-9};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="546" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img36.png"
+ ALT="\includegraphics{7c/lily-79db51c2-9}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-6};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="546" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-6}">|; 
+
+$key = q/includegraphics{daslashlily-e1c6fba5-6};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="538" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img42.png"
+ ALT="\includegraphics{da/lily-e1c6fba5-6}">|; 
+
+$key = q/includegraphics{64slashlily-a4d36519-3};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="538" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img22.png"
+ ALT="\includegraphics{64/lily-a4d36519-3}">|; 
+
+$key = q/includegraphics{daslashlily-e1c6fba5-1};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="115" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img37.png"
+ ALT="\includegraphics{da/lily-e1c6fba5-1}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-5};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="544" HEIGHT="79" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-5}">|; 
+
+$key = q/includegraphics{7cslashlily-79db51c2-6};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="546" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img33.png"
+ ALT="\includegraphics{7c/lily-79db51c2-6}">|; 
+
+$key = q/includegraphics{daslashlily-e1c6fba5-5};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="539" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img41.png"
+ ALT="\includegraphics{da/lily-e1c6fba5-5}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-10};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="547" HEIGHT="65" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-10}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-14};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="547" HEIGHT="68" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-14}">|; 
+
+$key = q/includegraphics{daslashlily-e1c6fba5-3};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="539" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img39.png"
+ ALT="\includegraphics{da/lily-e1c6fba5-3}">|; 
+
+$key = q/includegraphics{38slashlily-c18ae63d-2};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="531" HEIGHT="62" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img26.png"
+ ALT="\includegraphics{38/lily-c18ae63d-2}">|; 
+
+$key = q/includegraphics{7cslashlily-79db51c2-7};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="544" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img34.png"
+ ALT="\includegraphics{7c/lily-79db51c2-7}">|; 
+
+$key = q/includegraphics{7cslashlily-79db51c2-5};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="544" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img32.png"
+ ALT="\includegraphics{7c/lily-79db51c2-5}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-1};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="69" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-1}">|; 
+
+$key = q/includegraphics{b4slashlily-baaec67f-2};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="531" HEIGHT="60" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img48.png"
+ ALT="\includegraphics{b4/lily-baaec67f-2}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-8};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="546" HEIGHT="72" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-8}">|; 
+
+$key = q/flat;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="11" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img46.png"
+ ALT="$\flat$">|; 
+
+$key = q/includegraphics{64slashlily-a4d36519-4};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="539" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img23.png"
+ ALT="\includegraphics{64/lily-a4d36519-4}">|; 
+
+$key = q/includegraphics{daslashlily-e1c6fba5-2};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="531" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img38.png"
+ ALT="\includegraphics{da/lily-e1c6fba5-2}">|; 
+
+$key = q/includegraphics{baslashlily-62f3a9ed-15};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="546" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="\includegraphics{ba/lily-62f3a9ed-15}">|; 
+
+$key = q/includegraphics{86slashlily-16caec13-1};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="531" HEIGHT="59" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img19.png"
+ ALT="\includegraphics{86/lily-16caec13-1}">|; 
+
+1;
+
